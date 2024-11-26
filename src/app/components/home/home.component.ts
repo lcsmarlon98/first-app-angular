400e7cd5398beddfcb2a790e5ce19aed9d6b4085
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 19:43:33 -0300

    chamando a service do github no home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../services/github.service';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor (private http: GithubService) { }
+
+  ngOnInit(): void {
+    this.http.getUsers().subscribe(res => console.log(res))
+  }
+  
 }
