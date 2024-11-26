b79a991d483bc34d211fc9791281905ca5f2568f
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 18:13:34 -0300

    refatorando: correção ortográfica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
         path: 'home',
         component: HomeComponent
     }, {
-        path: 'seach/:username',
+        path: 'search/:username',
         component: SearchComponent
     }
 ];
