9bff3957b7e0efa9d763d169412191d595504655
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 18:41:41 -0300

    criando os métodos obrigatório da implementação da interface githubhttp

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { GithubHttpInterface } from '../interfaces/github-http.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GithubService {
+export class GithubService implements GithubHttpInterface {
 
   constructor() { }
+
+  getUsers(): HttpClient {
+    throw new Error('Method not implemented.');
+  }
+
+  getUserByUsername(): HttpClient {
+    throw new Error('Method not implemented.');
+  }
 }
