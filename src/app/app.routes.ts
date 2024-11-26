addf4f4f22e6dbd5c44210b92873a9f54017f845
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 18:12:29 -0300

    criando a rota de search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,8 @@ export const routes: Routes = [
     }, {
         path: 'home',
         component: HomeComponent
+    }, {
+        path: 'seach/:username',
+        component: SearchComponent
     }
 ];
