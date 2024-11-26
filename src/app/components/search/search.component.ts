a0181805e38f495c64d1b7e5451374549cc4d43b
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 20:49:34 -0300

    obtendo o parâmetro da url

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const username = this.route.snapshot.paramMap.get('username')
+    console.log(username)
+  }
 
 }
