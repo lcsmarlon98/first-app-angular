b4bc56b6bf907949b7c3d4af31021ffdedb0b8ad
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 18:11:41 -0300

    criando a rota home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,8 @@ export const routes: Routes = [
         path: '',
         redirectTo: 'home',
         pathMatch: 'full'
+    }, {
+        path: 'home',
+        component: HomeComponent
     }
 ];
