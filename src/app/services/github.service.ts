464bc9e19a4d1ffd2b7b8e281b81470bc01eba01
lcsmarlon <lcsmarlon@outlook.com>
2024-11-26 18:44:05 -0300

    criando o construtor com http

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GithubService implements GithubHttpInterface {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getUsers(): HttpClient {
     throw new Error('Method not implemented.');
